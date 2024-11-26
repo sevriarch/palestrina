adc235a6b6c08b090af668b9c23e3055d88cc24a
S <sevriarch@gmail.com>
2024-11-26 12:49:06 -0500

    update ci workflow; add missing midi file

--- misc/integration.sh
+++ misc/integration.sh
@@ -20,6 +20,6 @@ cd $(dirname $0)
 
 node ./midi-reader.js
 
-test_music_creation water1.new.js 8b605e819c08319007b585d2724ee404
-test_music_creation salope.js 1005ed2d433072460c5a43614009be90
+test_music_creation water1.new.js d0422047681ce6632afd75527f33c3ef
+test_music_creation danse.js 1005ed2d433072460c5a43614009be90
 test_music_creation pf-son-1-finale.js a66dc38696bf0df061dcf4e1db93e370
